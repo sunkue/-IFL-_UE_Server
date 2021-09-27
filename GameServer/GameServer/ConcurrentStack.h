4763b7ff7b38542d59999ff226dbba69b75332ae
sunkue <wmwwmmm@naver.com>
2021-09-27 19:28:25 +0900

    lockfree queue

--- GameServer/GameServer/ConcurrentStack.h
+++ GameServer/GameServer/ConcurrentStack.h
@@ -51,14 +51,63 @@ class LockFreeStack
 {
 	struct Node
 	{
-		Node(const T& value) :data{ value }
-		{
-
-		}
-
+		Node(const T& value) :data{ value } {};
 		T data;
 		Node* next = nullptr;
 	};
+
+	static void delete_nodes(Node* node)
+	{
+		while (node)
+		{
+			Node* temp = node->next;
+			delete node;
+			node = temp;
+		}
+	}
+
+	void try_delete(Node* target)
+	{
+		if (_pop_count == 1)
+		{
+			Node* node = _pending_list.exchange(nullptr);
+
+			if (0 == --_pop_count)
+			{
+				delete_nodes(node);
+			}
+			else if (node)
+			{
+				chain_pending_node_list(node);
+			}
+			delete target;
+		}
+		else
+		{
+			chain_pending_node(target);
+			--_pop_count;
+		}
+	}
+
+	void chain_pending_node_list(Node* first, Node* last)
+	{
+		last->next = _pending_list;
+		while (false == _pending_list.compare_exchange_weak(last->next, first))
+			;;;
+	}
+
+	void chain_pending_node_list(Node* node)
+	{
+		Node* last = node;
+		while (last->next) { last = last->next; }
+		chain_pending_node_list(node, last);
+	}
+
+	void chain_pending_node(Node* node)
+	{
+		chain_pending_node_list(node, node);
+	}
+
 
 public:
 
@@ -72,22 +121,29 @@ public:
 
 	bool try_pop(T& value)
 	{
+		++_pop_count;
+
 		Node* old_head = _head;
-		
-		while(old_head && false == _head.compare_exchange_weak(old_head, old_head->next))
+
+		while (old_head && false == _head.compare_exchange_weak(old_head, old_head->next))
 			;;;
 
-		if (nullptr == old_head) {
+		if (nullptr == old_head)
+		{
+			--_pop_count;
 			return false;
 		}
 
 		value = old_head->data;
-		// delete old_head;
+		try_delete(old_head);
 		return true;
 	}
+
 
 private:
 
 	atomic<Node*> _head;
 
+	atomic<uint32_t> _pop_count;
+	atomic<Node*> _pending_list;
 };
